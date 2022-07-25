1b619e02a370832c2b53dc8349b033a12032095e
Dmitriy <dmtn3611@gmail.com>
2022-07-25 19:22:45 +0300

    implement VariablesTheme version2

--- Lesson1/VariablesTheme.java
+++ Lesson1/VariablesTheme.java
@@ -1,6 +1,5 @@
-import java.util.Locale;
-
 public class VariablesTheme {
+
     public static void main(String[] args) {
         System.out.println("1. Создание переменных и вывод их значений на консоль\n");
         byte coreNumber = 6;
@@ -11,20 +10,25 @@ public class VariablesTheme {
         double winVer = 10.19044;
         char typeOfCPU = 'I';
         boolean isX64Based = true;
-        System.out.println("coreNumber = " + coreNumber + ", cpuFrequency = " + cpuFrequency + 
-                ", ramCapacity = " + ramCapacity + ", hdCapacity = " + hdCapacity);
-        System.out.println("biosVer = " + biosVer + ", winVer = " + winVer + ", typeOfCPU = " + 
-                typeOfCPU + ", isX64Based = " + isX64Based);
+        System.out.println("Количество ядер процессора : " + coreNumber);
+        System.out.println("Частота процессора : " + cpuFrequency);
+        System.out.println("Объем оперативной памяти : " + ramCapacity);
+        System.out.println("Объем памяти на жестком диске : " + hdCapacity);
+        System.out.println("Версия БИОС : " + biosVer);
+        System.out.println("Версия ОС Windows : " + winVer);
+        System.out.println("Тип процессора : " + typeOfCPU);
+        System.out.println("Принадлежность к 64-разрядной ОС : " + isX64Based);        
 
         System.out.println("\n2. Расчет стоимости товара со скидкой\n");
-        int x = 100;
-        int y = 200;
+        int xPrice = 100;
+        int yPrice = 200;
         int discount = 11;
-        int totalDiscount = (x + y) * 11 / 100;
+        int totalDiscount = (xPrice + yPrice) * 11 / 100;
         System.out.println("Сумма скидки = " + totalDiscount);
-        System.out.println("Общая стоимость товаров со скидкой = " + (300 - totalDiscount));
+        System.out.println("Общая стоимость товаров со скидкой = " + (xPrice + yPrice - 
+                totalDiscount));
 
-        System.out.println("\n3. Вывод на консоль слова JAVA");
+        System.out.println("\n3. Вывод на консоль слова JAVA\n");
         System.out.println("    J    a  v     v  a    ");
         System.out.println("    J   a a  v   v  a a   ");
         System.out.println(" J  J  aaaaa  V V  aaaaa  ");
@@ -35,58 +39,44 @@ public class VariablesTheme {
         short maxShort = 32767;
         int maxInt = Integer.MAX_VALUE;
         long maxLong = Long.MAX_VALUE;
-        System.out.println(String.format("maxByte = %d, maxShort = %d, maxInt = %d, maxLong = %d",
-                maxByte, maxShort, maxInt, maxLong));
-        maxByte++;
-        maxShort++;
-        maxInt++;
-        maxLong++;
-        System.out.println(String.format("maxByte = %d, maxShort = %d, maxInt = %d, maxLong = %d",
-                maxByte, maxShort, maxInt, maxLong));
-        maxByte--;
-        maxShort--;
-        maxInt--;
-        maxLong--;
-        System.out.println(String.format("maxByte = %d, maxShort = %d, maxInt = %d, maxLong = %d",
-                maxByte, maxShort, maxInt, maxLong));
+        System.out.printf("maxByte = %d, maxShort = %d, maxInt = %d, maxLong = %d\n", maxByte, 
+                maxShort, maxInt, maxLong);        
+        System.out.printf("maxByte = %d, maxShort = %d, maxInt = %d, maxLong = %d\n", ++maxByte, 
+                ++maxShort, ++maxInt, ++maxLong);        
+        System.out.printf("maxByte = %d, maxShort = %d, maxInt = %d, maxLong = %d\n", --maxByte,
+                --maxShort, --maxInt, --maxLong);
 
         System.out.println("\n5. Перестановка значений переменных\n");
-        int firstInt = 1;
-        int secondInt = 2;        
+        int num1 = 1;
+        int num2 = 2;        
         System.out.println("Перестановка с помощью третьей переменной");
-        System.out.println(String.format(Locale.ROOT, "Исходные значения переменных: %d, %d", 
-                firstInt, secondInt));
-        int thirdInt = firstInt;
-        firstInt = secondInt;
-        secondInt = thirdInt;
-        System.out.println(String.format(Locale.ROOT, "Новые значения переменных: %d, %d\n", 
-                firstInt, secondInt));
-        System.out.println("Перестановка с помощью арифметической операции");
-        System.out.println(String.format(Locale.ROOT, "Исходные значения переменных: %d, %d", 
-                firstInt, secondInt));        
-        firstInt += secondInt;
-        secondInt = firstInt - secondInt;
-        firstInt -= secondInt;  
-        System.out.println(String.format(Locale.ROOT, "Новые значения переменных: %d, %d\n", 
-                firstInt, secondInt));
-        System.out.println("Перестановка с помощью побитовой операции ^");
-        System.out.println(String.format(Locale.ROOT, "Исходные значения переменных: %d, %d", 
-                firstInt, secondInt));
-        firstInt = firstInt ^ secondInt;
-        secondInt = secondInt ^ firstInt;
-        firstInt = firstInt ^ secondInt;
-        System.out.println(String.format(Locale.ROOT, "Новые значения переменных: %d, %d", 
-                firstInt, secondInt));
+        System.out.printf("Исходные значения переменных: %d, %d", num1, num2);
+        int buffer = num1;
+        num1 = num2;
+        num2 = buffer;
+        System.out.printf("\nНовые значения переменных: %d, %d\n", num1, num2);
+        System.out.println("\nПерестановка с помощью арифметической операции");
+        System.out.printf("Исходные значения переменных: %d, %d", num1, num2);        
+        num1 += num2;
+        num2 = num1 - num2;
+        num1 -= num2;  
+        System.out.printf("\nНовые значения переменных: %d, %d\n", num1, num2);
+        System.out.println("\nПерестановка с помощью побитовой операции ^");
+        System.out.printf("Исходные значения переменных: %d, %d", num1, num2);
+        num1 ^= num2;
+        num2 ^= num1;
+        num1 ^= num2;
+        System.out.printf("\nНовые значения переменных: %d, %d\n", num1, num2);
 
         System.out.println("\n6. Вывод символов и их кодов\n");
-        int intOne = 35;
-        int intTwo = 38;
-        int intThree = 64;
-        int intFour = 94;
-        int intFive = 95;
-        System.out.println(String.format("%d %c, %d %c, %d %c, %d %c, %d %c", intOne, (char) intOne,
-                intTwo, (char) intTwo, intThree, (char) intThree, intFour, (char) intFour, intFive, 
-                (char) intFive));
+        char codeOne = 35;
+        char codeTwo = 38;
+        char codeThree = 64;
+        char codeFour = 94;
+        char codeFive = 95;
+        System.out.printf("%d %c\n%d %c\n%d %c\n%d %c\n%d %c\n", (int) codeOne, codeOne, 
+                (int) codeTwo, codeTwo, (int) codeThree, codeThree, (int) codeFour, codeFour,
+                (int) codeFive, codeFive); 
 
         System.out.println("\n7. Произведение и сумма цифр числа\n");
         int srcNum = 345;
@@ -104,27 +94,29 @@ public class VariablesTheme {
         char leftBracket ='(';
         char rightBracket = ')';
         char underscore = '_';
-        System.out.println(String.format("    %c%c    ", backSlash, slash));
-        System.out.println(String.format("   %c  %c   ", backSlash, slash));
-        System.out.println(String.format("  %c%c%c %c%c",backSlash, underscore, leftBracket, 
-                rightBracket, slash));
-        System.out.println(String.format(" %c      %c ", backSlash, slash));
-        System.out.println(String.format("%c%c%c%c%c%c%c%c%c%c", backSlash, underscore, underscore,
-                underscore, underscore, backSlash, slash, underscore, underscore, slash));
+        char space = ' ';
+        System.out.printf("%c%c%c%c%c%c\n", space, space, space, space, backSlash, slash);
+        System.out.printf("%c%c%c%c%c%c%c\n", space, space, space, backSlash, space, space, slash);
+        System.out.printf("%c%c%c%c%c%c%c%c\n", space, space, backSlash, underscore, leftBracket, 
+                space, rightBracket, slash);
+        System.out.printf("%c%c%c%c%c%c%c%c%c\n", space, backSlash, space, space, space, 
+                space, space, space, slash);
+        System.out.printf("%c%c%c%c%c%c%c%c%c%c\n", backSlash, underscore, underscore,
+                underscore, underscore, backSlash, slash, underscore, underscore, slash);
 
         System.out.println("\n9. Отображение количества сотен, десятков и единиц числа\n");
         srcNum = 123;
         firstDigit = srcNum / 100;
         secondDigit = (srcNum / 10) % 10;
-        thirdDigit = srcNum % 120;
-        System.out.println(String.format("Число %d содержит:\n%d сотню\n%d десятка\n%d единицы", 
-                srcNum, firstDigit, secondDigit, thirdDigit));
+        thirdDigit = srcNum % 10;
+        System.out.printf("Число %d содержит:\n%d сотен\n%d десятков\n%d единиц\n", srcNum, 
+                firstDigit, secondDigit, thirdDigit);
 
         System.out.println("\n10. Преобразование секунд");
         int totalSeconds = 86399;
         int hours = totalSeconds / 3600;
         int minutes = (totalSeconds % 3600) / 60; 
-        int seconds = (totalSeconds % 3600) % 60;
-        System.out.println(String.format("%d:%d:%d", hours, minutes, seconds));
+        int seconds = totalSeconds % 60;
+        System.out.printf("%d:%d:%d", hours, minutes, seconds);
     }
 }
